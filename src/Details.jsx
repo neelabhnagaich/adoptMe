dd6a998f3919b64f634bf654de9e4d98f72631d6
neelabh nagaich <neel@neelabhs-MacBook-Air.local>
2023-02-17 09:37:02 +0530

    portal implemented

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,30 +1,54 @@
-import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
+import Carousel from "./Carousel";
+import Modal from "./Modal";
+import { useState } from "react";
 
 const Details = () => {
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
+  const [showModal, setShowModal] = useState(false)
 
-  if(results.isLoading){
+  if (results.isLoading) {
     return (
-      <div className="loading-pand">
-        <h2 className="loader">Loading...</h2>
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
       </div>
-    )
+    );
   }
 
   const pet = results.data.pets[0];
+
   return (
     <div className="details">
-      <div className="">
+      <Carousel images={pet.images} />
+      <div>
         <h1>{pet.name}</h1>
-        <h2>{pet.animal} - {pet.breed} - {pet.city} - {pet.state}</h2>
-        <button>Adopt {pet.name}</button>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button onClick={()=> {setShowModal(true)}}>Adopt {pet.name}</button>
         <p>{pet.description}</p>
+        {showModal ? 
+        <Modal>
+          <div>
+            <h1>Would you like to adopt {pet.name}?</h1>
+            <div className="buttons">
+              <button onClick={()=> {setShowModal(false)}}>yes</button>
+              <button onClick={()=> {setShowModal(false)}}>Close</button>
+            </div>
+          </div>
+        </Modal> 
+        : null}
       </div>
     </div>
-  )
+  );
 };
 
-export default Details;
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,31 @@
+import {Component} from 'react'
+import { Link } from 'react-router-dom';
+
+class ErrorBoundary extends Component {
+    state = {hasError : false};
+
+    static getDerivedStateFromError() {
+        return {hasError: true};
+    }
+
+    componentDidCatch(error, info){
+        // typically you would log this to something like trackjs or NewRelic
+        console.error("errorboundary component cought as error", error, info);
+    }
+
+    render (){
+        if(this.state.hasError){
+            return (
+                <h2>
+                    There was an error with this listing,
+                    <Link to='/'>Click here to go back to the home Page</Link>
+                </h2>
+            )
+        }
+
+        return this.props.children;
+    }
+}
+
+
+export default ErrorBoundary;
